2edfde1eff3fd514c1ae490363da0889ec204c6e
ok1402s <2152183kOOOdd###42>
2022-03-28 22:14:35 +0300

    AuthController + AuthRoutes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
 Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 Route::get('/isloggedin', [App\Http\Controllers\AuthController::class, 'isloggedin'])->name('haslogin');
+
