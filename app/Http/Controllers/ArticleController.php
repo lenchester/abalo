c5fe6936461c22c01337119a0bbaff3b6bd249e9
lenchester <57565463+lenchester@users.noreply.github.com>
2022-03-31 00:21:08 +0200

    Implemented search by name(case-sensitive)

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\ab_article;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+
+class ArticleController extends Controller
+{
+    public function index(Request $request){
+        $search = $request->get('search');
+        if($search) {
+            $articles = DB::table('ab_articles')->select('ab_name')->where('ab_name', 'LIKE','%'.$search.'%')->get();
+        } else {
+            $articles = ab_article::all();
+        }
+        return view('articles',['articles'=>$articles]);
+    }
+}
